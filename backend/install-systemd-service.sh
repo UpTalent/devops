97ad6f826cfa4e3cc30f5be944b8cda480776a8b
ViktorVoloshko <viktor.voloshko@outlook.com>
2023-03-23 03:46:03 +0200

    Initial prod-ready CI

--- backend/install-systemd-service.sh
+++ backend/install-systemd-service.sh
@@ -3,7 +3,7 @@
 if [ -f /etc/systemd/system/provedcode.service ]; then
     echo "systemd unit already installed!"
 else
-    sudo cp ./backend/provedcode.service /etc/systemd/system/provedcode.service
+    sudo cp ./backend/uptalentbackend.service /etc/systemd/system/uptalentbackend.service
     sudo systemctl daemon-reload
     echo "systemd unit installed!"
 fi
